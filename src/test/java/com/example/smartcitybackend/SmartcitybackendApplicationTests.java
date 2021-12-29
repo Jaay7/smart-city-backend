e3571d616d27bfc4edca80df7cfff9433d720d79
Jaay7 <jayakrishna.madem@gmail.com>
2021-12-29 09:23:38 +0530

    removed tests

--- src/test/java/com/example/smartcitybackend/SmartcitybackendApplicationTests.java
+++ src/test/java/com/example/smartcitybackend/SmartcitybackendApplicationTests.java
@@ -1,13 +1,5 @@
 package com.example.smartcitybackend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class SmartcitybackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
